1895e6d0d02e60f8fb506dc6ba7bc0e7a791c230
cdapaaaddo@gmail.com <cdapaaaddo@gmail.com>
2018-06-07 21:19:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,9 +15,13 @@ function whileLoop(n) {
   } return 'done'
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
 function doWhileLoop(array) {
   do {
     jh;
-  } while ()
-  return
+  } while (maybeTrue())
+  return array
 }
